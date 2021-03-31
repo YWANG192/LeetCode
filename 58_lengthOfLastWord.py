ddc019197b22bd3f9d01c5e7571142f6a1a3cae0
YWANG192 <ianwang7152@gmail.com>
2021-03-31 11:45:16 -0500

    Create 58_lengthOfLastWord.py

---
+++ 58_lengthOfLastWord.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+        if s=="" :
+            return 0
+
+        split_list=s.split()
+        
+        
+        if len(split_list)==0:
+            return 0
+        
+        last_word=split_list[-1]
+        
+        return len(last_word)
