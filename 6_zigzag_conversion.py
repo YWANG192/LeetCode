00f048641aadc56ff7d2a4dac0969c53c91bce25
YWANG192 <ianwang7152@gmail.com>
2021-04-01 12:11:32 -0500

    Create 6_zigzag_conversion.py

---
+++ 6_zigzag_conversion.py
@@ -0,0 +1,24 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        delta = -1
+        row = 0        
+        res = [[] for i in range(numRows)]
+        print(res)
+        
+        for c in s:
+            res[row].append(c)
+            
+            if row == 0 or row == numRows -1:
+                delta *= -1
+            
+            row += delta
+        
+        for i in range (len(res)):
+            res[i] = ''.join(res[i])
+    
+        return ''.join(res)
