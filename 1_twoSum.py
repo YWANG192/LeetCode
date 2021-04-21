07a2bdac4ee724c821c968839def27fe31eade94
YWANG192 <ianwang7152@gmail.com>
2021-04-21 16:40:10 -0500

    Update and rename twoSum.py to 1_twoSum.py

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -1,14 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        if len(nums) == None:
-            
+        if nums is None:
             return []
         
         my_dict = {}
         
         for i in range(len(nums)):
+            
             if nums[i] not in my_dict:
-                my_dict[target - nums[i]] = i
+                my_dict [target - nums[i]] = i
+            
             else:
-                return [i, my_dict[nums[i]]]
+                return [my_dict [nums[i]], i]
