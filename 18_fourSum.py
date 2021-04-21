b648621e6b38f89ef37347c01e3c1ad6ac92904b
YWANG192 <ianwang7152@gmail.com>
2021-04-21 18:06:45 -0500

    Update 18_fourSum.py

--- 18_fourSum.py
+++ 18_fourSum.py
@@ -1,40 +1,42 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
         if not nums or len(nums) < 4:
             return []
-        
-        result = []
+
+        res = []
         nums.sort()
-        
-        for i in range(len(nums) - 3):
+
+        for i in range (len(nums) - 3):
+
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
-            for j in range(i+1, len(nums) - 2):
+
+            for j in range (i+1, len(nums) - 2):
+
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                
-                sumA = nums[i] + nums[j]
-                left = j+1
-                right = len(nums) - 1
-                
-                while left < right:
-                    
-                    sumB = sumA + nums[left] + nums[right]
-                    
-                    if sumB == target:
-                        result.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        
-                        while left < right and nums[left] == nums[left -1]:
-                            left +=1
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-                    
-                    elif sumB < target:
-                        left +=1
+
+
+                l, r = j+1, len(nums) - 1
+
+                while l < r:
+
+                    temp_sum = nums[i] + nums[j] + nums[l] + nums[r]
+
+                    if temp_sum == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+
+                        l += 1
+
+                        if l < r and nums[l] == nums[l-1]:
+                            l += 1
+
+                    elif temp_sum < target:
+                        l += 1
+
                     else:
-                        right -= 1
-        return result
+                        r -= 1
+                        
+        return res
                         
