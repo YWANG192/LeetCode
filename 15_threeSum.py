d37c5e176977d733b157cd95bd5c728359a382ff
YWANG192 <ianwang7152@gmail.com>
2021-04-21 17:26:15 -0500

    Update 15_threeSum.py

--- 15_threeSum.py
+++ 15_threeSum.py
@@ -1,6 +1,9 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
+        if not nums or len(nums) < 3:
+            return []
+        
         nums.sort()
         res = []
         
