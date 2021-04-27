ab2bc245ddfd032e3f2407f4ede087e475b8dc76
YWANG192 <ianwang7152@gmail.com>
2021-04-27 12:10:59 -0500

    Update 3_lengthOfLongestSubstring.py

--- 3_lengthOfLongestSubstring.py
+++ 3_lengthOfLongestSubstring.py
@@ -1,29 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        i = 0
+        j = 0
         my_dict = {}
         max_len = 0
         
-        
-        
-        for j in range(len(s)):
+        for i in range (len(s)):
             
-            if not s[j] in my_dict:
+            if not s[i] in my_dict:
                 
-                my_dict[s[j]] = j
+                my_dict[s[i]] = i
                 
             else:
                 
-                if i < my_dict[s[j]] + 1:
-                    i = my_dict[s[j]] + 1
+                if j < my_dict[s[i]] + 1:
+                    j =  my_dict[s[i]] + 1
                 
-                my_dict[s[j]] = j
-                
-            temp_len = j-i+1
-
+                my_dict[s[i]] = i
+            
+            temp_len = i - j + 1
+            
             if temp_len > max_len:
-                max_len = temp_len
                 
-
+                max_len = temp_len
+        
         return max_len
+            
